dc587061c4561bf79d30d2c387ade5d6d0c89e5a
Derek Rushing <20804720+drush211@users.noreply.github.com>
2026-02-18 14:00:43 -0600

    INIT-442: Get a working build for a single typescript file.

---
+++ src/env/env.ts
@@ -0,0 +1,15 @@
+export const Env = {
+    Development: 'development',
+    Staging: 'staging',
+    Production: 'production',
+} as const;
+
+export type Env = (typeof Env)[keyof typeof Env];
+
+export function isDevelopment(env: Env): boolean {
+    return env === Env.Development;
+}
+
+export function isNonDevelopment(env: Env): boolean {
+    return !isDevelopment(env);
+}
