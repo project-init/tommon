9a4e55b204c7da02bc706d238802b8b017c9bf84
zigenis <132610996+zigenis@users.noreply.github.com>
2026-03-03 15:24:15 -0800

    Appeasing the arbiter of formatting

--- src/grpc/index.ts
+++ src/grpc/index.ts
@@ -1,8 +1,15 @@
 import type { Transport } from '@connectrpc/connect';
-import { createGrpcTransport, type GrpcTransportOptions } from '@connectrpc/connect-node';
+import {
+  createGrpcTransport,
+  type GrpcTransportOptions,
+} from '@connectrpc/connect-node';
 import { isDevelopment, type Env } from '../env';
 
-export function createGrpcTransportForEnv(host: string, env: Env, options?: GrpcTransportOptions): Transport {
+export function createGrpcTransportForEnv(
+  host: string,
+  env: Env,
+  options?: GrpcTransportOptions,
+): Transport {
   const baseUrl = isDevelopment(env) ? `http://${host}` : `https://${host}`;
 
   return createGrpcTransport({ baseUrl, ...options });
