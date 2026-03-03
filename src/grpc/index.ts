c4d2d74b77e3fe046155e49af9bd89bf206a2a3c
zigenis <132610996+zigenis@users.noreply.github.com>
2026-03-03 15:02:29 -0800

    Updated createGrpcTransportForEnv to accept optional...options

--- src/grpc/index.ts
+++ src/grpc/index.ts
@@ -1,9 +1,9 @@
 import type { Transport } from '@connectrpc/connect';
-import { createGrpcTransport } from '@connectrpc/connect-node';
+import { createGrpcTransport, type GrpcTransportOptions } from '@connectrpc/connect-node';
 import { isDevelopment, type Env } from '../env';
 
-export function createGrpcTransportForEnv(host: string, env: Env): Transport {
+export function createGrpcTransportForEnv(host: string, env: Env, options?: GrpcTransportOptions): Transport {
   const baseUrl = isDevelopment(env) ? `http://${host}` : `https://${host}`;
 
-  return createGrpcTransport({ baseUrl });
+  return createGrpcTransport({ baseUrl, ...options });
 }
